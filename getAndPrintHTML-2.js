b083590e1776aad7a7fb02279350f302dfeaf603
DavidFok <david_fok@hotmail.com>
2018-01-23 01:25:04 +0000

    modulated getAndPrintHTML

---
+++ getAndPrintHTML-2.js
@@ -0,0 +1,25 @@
+var https = require('https');
+
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step3.html'
+  };
+
+function getAndPrintHTML (options) {
+  https.get(requestOptions, function (response) {
+    response.setEncoding('utf8');
+
+    var dataList = [];
+    response.on('data', function (data) {
+      dataList.push(data);
+    });
+
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    response.on('end', function() {
+      console.log(dataList.join(''));
+    });
+  });
+}
+
+getAndPrintHTML (requestOptions);
